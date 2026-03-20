9e7066acb36719f313791c94a1393ead72f45675
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-20 11:58:40 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 28 ms, Memory: 7296 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
+const int MAX = 1e6;
 int n;
+int nmaximum;
 int nlist[100010];
-bool nexist[1000010];
-int ans[1000010];
+bool nexist[MAX + 10];
+int ans[MAX + 10];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -14,11 +16,12 @@ int main() {
 	for (int i = 0;i < n;i++) {
 		cin >> nlist[i];
 		nexist[nlist[i]] = 1;
+		if (nmaximum < nlist[i]) nmaximum = nlist[i];
 	}
 
 	for (int i = 0;i < n;i++) {
 		int x = nlist[i];
-		for (int j = x * 2;j <= 1000000;j += x) {
+		for (int j = x * 2;j <= nmaximum;j += x) {
 			if (nexist[j]) {
 				ans[j]--;
 				ans[x]++;
