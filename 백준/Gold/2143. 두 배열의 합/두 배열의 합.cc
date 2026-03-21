9b61bfee153b132a303815e7b29c995ef060eea5
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-21 17:08:34 +0900

    [Gold III] Title: 두 배열의 합, Time: 64 ms, Memory: 8304 KB -BaekjoonHub

---
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1e3 + 10;
+int t, n, m;
+int nlist[MAX];
+int mlist[MAX];
+vector<int> nsum;
+vector<int> msum;
+long long int ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> t;
+	
+	cin >> n;
+	for (int i = 0;i < n;i++) cin >> nlist[i];
+	for (int i = 0;i < n;i++) {
+		int sum = 0;
+		for (int j = i;j < n;j++) {
+			sum += nlist[j];
+			nsum.push_back(sum);
+		}
+	}
+	sort(nsum.begin(), nsum.end());
+
+	cin >> m;
+	for (int i = 0;i < m;i++) cin >> mlist[i];
+	for (int i = 0;i < m;i++) {
+		int sum = 0;
+		for (int j = i;j < m;j++) {
+			sum += mlist[j];
+			msum.push_back(sum);
+		}
+	}
+	sort(msum.begin(), msum.end());
+
+	int nidx = 0, midx = msum.size() - 1;
+
+	while (nidx < nsum.size() && midx >= 0) {
+		int temp = nsum[nidx] + msum[midx];
+		if (temp == t) {
+			int ncnt = 0, mcnt = 0;
+			int nval = nsum[nidx], mval = msum[midx];
+			while (nidx < nsum.size() && nsum[nidx] == nval) {
+				ncnt++;
+				nidx++;
+			}
+			while (midx >= 0 && msum[midx] == mval) {
+				mcnt++;
+				midx--;
+			}
+			ans += (long long int) ncnt * mcnt;
+		}
+		else {
+			if (temp > t) midx--;
+			else nidx++;
+		}
+	}
+	
+	cout << ans;
+
+	return 0;
+}
