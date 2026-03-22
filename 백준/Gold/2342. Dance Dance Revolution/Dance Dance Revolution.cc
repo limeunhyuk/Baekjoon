a2d1988eb0b5ef7344d7fdc758f02ed573348e32
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-22 13:16:25 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int cost(int start, int end) {
+	if (start == end) return 1;
+	if (!start || !end) return 2;
+	if (abs(start - end) % 2) return 3;
+	return 4;
+}
+
+int Foot;
+int dp[2][5];
+bool flip;
+const int MAX = 1e9;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+	for (int i = 0;i < 5;i++) {
+		dp[0][i] = MAX;
+		dp[1][i] = MAX;
+	}
+	dp[0][0] = 0;
+
+	while (n) {
+		for (int i = 0;i < 5;i++) dp[!flip][i] = MAX;
+
+		for (int i = 0;i < 5;i++) {
+			if (dp[flip][i] == MAX) continue;
+			dp[!flip][i] = min(dp[!flip][i], dp[flip][i] + cost(Foot, n));
+			dp[!flip][Foot] = min(dp[!flip][Foot], dp[flip][i] + cost(i, n));
+		}
+
+		Foot = n;
+		flip = !flip;
+		cin >> n;
+	}
+
+	int ans = MAX;
+	for (int i = 0;i < 5;i++) {
+		if (dp[flip][i] < ans) ans = dp[flip][i];
+	}
+	cout << ans;
+
+	return 0;
+}
