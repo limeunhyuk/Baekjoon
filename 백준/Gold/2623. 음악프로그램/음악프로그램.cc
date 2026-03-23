d86f9b07fc8ffb9560dd243d1b31fe60a57293c3
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-23 12:04:09 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -5,9 +5,9 @@
 using namespace std;
 
 const int max_n = 1000;
-vector<int> v[max_n + 10];
+vector<int> adj[max_n + 10];
 vector<int> ans;
-int cnt[max_n + 10];
+int in_degree[max_n + 10];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -24,25 +24,26 @@ int main() {
 		cin >> idx1;
 		for (int j = 1;j < k;j++) {
 			cin >> idx2;
-			v[idx1].push_back(idx2);
-			cnt[idx2]++;
+			adj[idx1].push_back(idx2);
+			in_degree[idx2]++;
 			idx1 = idx2;
 		}
 	}
 
 	queue<int> q;
-	for (int i = 1;i <= n;i++) if (!cnt[i]) q.push(i);
+	for (int i = 1;i <= n;i++) if (!in_degree[i]) q.push(i);
 
+	// 위상정렬
 	while (!q.empty()) {
 		int cur_n = q.front();
 		ans.push_back(cur_n);
 		q.pop();
-		for (int i = 0;i < v[cur_n].size();i++) {
-			int next_n = v[cur_n][i];
-			cnt[next_n]--;
-			if (cnt[next_n] == 0) q.push(next_n);
+		for (int next_n : adj[cur_n]) {
+			in_degree[next_n]--;
+			if (in_degree[next_n] == 0) q.push(next_n);
 		}
 	}
+	// 만약 결과가 n개가 아닌 경우는 안에서 사이클 발생이라는 의미
 	if (ans.size() == n) for (int i = 0;i < n;i++) cout << ans[i] << "\n";
 	else cout << 0;
 
