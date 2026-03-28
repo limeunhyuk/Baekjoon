79d7c71754d9241b2a047940cdfe1a9f72182760
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-28 10:01:24 +0900

    [Gold I] Title: 계단 수, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 1024;
+const int MAX2 = 1e9;
+int n;
+long long dp[2][10][MAX];
+// 메모리 절약을 위해 cur을 idx로 2개의 배열을 번갈아가며 사용한다.
+bool cur = 1;
+
+// 기존 visited에 새로 들어온 x를 추가해서 반환한다.
+int addbit(int len, int x) {
+	return len | (1 << x);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	for (int j = 1;j < 10;j++) dp[cur][j][addbit(0, j)] = 1;
+
+	for (int i = 2;i <= n;i++) {
+		for (int j = 0;j < 10;j++) {
+			for (int k = 0;k < MAX;k++) {
+				if (j - 1 >= 0) {
+					dp[!cur][j - 1][addbit(k, j - 1)] += dp[cur][j][k];
+					dp[!cur][j - 1][addbit(k, j - 1)] %= MAX2;
+				}
+				if (j + 1 < 10) {
+					dp[!cur][j + 1][addbit(k, j + 1)] += dp[cur][j][k];
+					dp[!cur][j + 1][addbit(k, j + 1)] %= MAX2;
+				}
+			}
+		}
+		for (int j = 0;j < 10;j++) {
+			for (int k = 0;k < MAX;k++) {
+				dp[cur][j][k] = 0;
+			}
+		}
+		cur = !cur;
+	}
+
+	long long ans = 0;
+	for (int i = 0;i < 10;i++) {
+		ans += dp[cur][i][MAX - 1];
+		ans %= MAX2;
+	}
+	cout << ans;
+
+	return 0;
+}
