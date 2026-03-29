561edd47910ef1e995937b195fc139a6d2b31c4a
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-03-29 13:48:06 +0900

    [Gold I] Title: 열쇠, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.cc
+++ 백준/Gold/9328. 열쇠/열쇠.cc
@@ -2,55 +2,39 @@
 #include <cstring>
 #include <queue>
 #include <vector>
+#include <string>
 using namespace std;
 
-const int MAX = 100;
-int t;
-int h, w;
-string map[MAX];
-queue<pair<int, int>> visit;
+const int MAX = 110;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-bool haveKey[26];
-bool visited[MAX][MAX];
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-
+	
+	int t;
 	cin >> t;
 	while (t--) {
+		int h, w;
 		cin >> h >> w;
 
-		// 테스트케이스 변수 초기화
+		char map[MAX][MAX];
+		queue<pair<int, int>> visit;
 		vector<pair<int, int>> keyLoca[26];
-		for (int i = 0;i < 26;i++) haveKey[i] = 0;
-		for (int i = 0;i < h;i++) {
-			for (int j = 0;j < w;j++) {
-				visited[i][j] = 0;
-			}
-		}
+		bool haveKey[26];
+		bool visited[MAX][MAX];
+
+		// 테스트케이스 변수 초기화
+		memset(haveKey, 0, sizeof(haveKey));
+		memset(visited, 0, sizeof(visited));
+		memset(map, '.', sizeof(map));
 		
-		for (int i = 0;i < h;i++) {
-			cin >> map[i];
-			// 최초 진입 가능 지점 확인
-			if (i == 0 || i == h - 1) {
-				for (int j = 0;j < w;j++) {
-					if (map[i][j] != '*') {
-						visit.push({ i,j });
-						visited[i][j] = 1;
-					}
-				}
-			}
-			else {
-				if (map[i][0] != '*') {
-					visit.push({ i,0 });
-					visited[i][0] = 1;
-				}
-				if (map[i][w - 1] != '*') {
-					visit.push({ i,w - 1 });
-					visited[i][w - 1] = 1;
-				}
+		for (int i = 1;i <= h;i++) {
+			string s;
+			cin >> s;
+			for (int j = 1;j <= w;j++) {
+				map[i][j] = s[j - 1];
 			}
 		}
 
@@ -58,37 +42,42 @@ int main() {
 		cin >> s;
 		if (s != "0") for (char i : s) haveKey[i - 'a'] = 1;
 
+		visit.push({ 0,0 });
+		visited[0][0] = 1;
+
 		int ans = 0;
 		while (!visit.empty()) {
 			pair<int, int> cur = visit.front();
 			visit.pop();
 			int curX = cur.first;
 			int curY = cur.second;
-			char c = map[curX][curY];
-			
-			if ('A' <= c && c <= 'Z') {
-				if (!haveKey[c - 'A']) {
-					keyLoca[c - 'A'].push_back({ curX,curY });
-					continue;
-				}
-			}
-			if ('a' <= c && c <= 'z') {
-				haveKey[c - 'a'] = 1;
-				for (auto i : keyLoca[c - 'a']) {
-					visit.push({ i.first,i.second });
-				}
-			}
-			if (c == '$') ans++;
 
-			map[curX][curY] = '*';
 			for (int i = 0;i < 4;i++) {
-				int nextX = curX + dx[i];
-				int nextY = curY + dy[i];
-				if (0 <= nextX && nextX < h && 0 <= nextY && nextY < w) {
-					if (map[nextX][nextY] != '*' && !visited[nextX][nextY]) {
-						visit.push({ nextX, nextY });
-						visited[nextX][nextY] = 1;
+				int nX = curX + dx[i];
+				int nY = curY + dy[i];
+
+				if (0 <= nX && nX <= h + 1 && 0 <= nY && nY <= w + 1) {
+					if (map[nX][nY] == '*' || visited[nX][nY]) continue;
+
+					char c = map[nX][nY];
+					visited[nX][nY] = 1;
+
+					if ('A' <= c && c <= 'Z') {
+						if (!haveKey[c - 'A']) {
+							keyLoca[c - 'A'].push_back({ nX,nY });
+							continue;
+						}
 					}
+					else if ('a' <= c && c <= 'z') {
+						haveKey[c - 'a'] = 1;
+						for (auto i : keyLoca[c - 'a']) {
+							visit.push({ i.first,i.second });
+						}
+						keyLoca[c - 'a'].clear();
+					}
+					else if (c == '$') ans++;
+
+					visit.push({ nX, nY });
 				}
 			}
 		}
