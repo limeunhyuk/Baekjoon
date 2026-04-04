9443b27f66b163b4e97f7d4fab254125a7b4d83c
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-04-04 13:56:58 +0900

    [Gold I] Title: 카드 게임, Time: 712 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Gold/16566. 카드 게임/카드 게임.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+const int MAX = 4000010;
+int card[MAX];
+int parent[MAX];
+
+int n, m, k;
+
+int find(int a) {
+	if (parent[a] == a) return a;
+	return parent[a] = find(parent[a]);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	cin >> n >> m >> k;
+	for (int i = 0;i < m;i++) {
+		cin >> card[i];
+		parent[i] = i;
+	}
+	parent[m] = m;
+	sort(card, card + m );
+
+	for (int i = 0;i < k;i++) {
+		int temp;
+		cin >> temp;
+		int idx = find(upper_bound(card, card + m, temp) - card);
+		cout << card[idx] << "\n";
+		parent[idx] = idx + 1;
+	}
+
+	return 0;
+}
