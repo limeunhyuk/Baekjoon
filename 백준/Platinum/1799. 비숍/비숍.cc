70a664a9b7dbae5d08976b930b78d7b2fb8753bd
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-04-07 17:57:48 +0900

    [Platinum V] Title: 비숍, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/1799. 비숍/비숍.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+bool map[11][11];
+bool neg[20];
+int ans[2];
+
+void dfs(int pos, int num, bool odd) {
+    if (pos > 2 * n - 2) {
+        if (ans[odd] < num) ans[odd] = num;
+        return;
+    }
+    for (int i = max(0, pos - n + 1);i <= min(pos, n - 1);i++) {
+        int j = pos - i;
+        if (map[i][j] && !neg[i - j + n]) {
+            neg[i - j + n] = 1;
+            dfs(pos + 2, num + 1, odd);
+            neg[i - j + n] = 0;
+        }
+    }
+    dfs(pos + 2, num, odd);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for (int i = 0;i < n;i++) {
+        for (int j = 0;j < n;j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    dfs(0, 0, 0);
+    dfs(1, 0, 1);
+    cout << ans[0] + ans[1];
+
+    return 0;
+}
