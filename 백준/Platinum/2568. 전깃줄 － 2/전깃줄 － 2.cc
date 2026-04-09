72537d9174580d63f716d838613770cadd15b396
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-04-09 12:21:40 +0900

    [Platinum V] Title: 전깃줄 - 2, Time: 40 ms, Memory: 5556 KB -BaekjoonHub

--- 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
+++ 백준/Platinum/2568. 전깃줄 － 2/전깃줄 － 2.cc
@@ -6,7 +6,7 @@ using namespace std;
 int n;
 vector<pair<int, int>> v;
 int lis[100010];
-int pnt[500010];
+int lis_loca[500010];
 bool check[500010];
 int max_num;
 
@@ -17,25 +17,27 @@ int main() {
     cin >> n;
     v.reserve(n);
     for (int i = 0;i < n;i++) {
-        v.push_back({ 0,0 });
-        cin >> v[i].second >> v[i].first;
-        if (max_num < v[i].second) max_num = v[i].second;
-        check[v[i].second] = 1;
+        int a, b;
+        cin >> a >> b;
+        // A 기준으로 출력하기 위해 바꿔서 넣는다.
+        v.push_back({ b,a });
+        if (max_num < a) max_num = a;
+        check[a] = 1;
     }
     sort(v.begin(), v.end());
     
     int min_lines = 0;
     for (int i = 0;i < n;i++) {
-        int temp = upper_bound(lis, lis + min_lines, v[i].second) - lis;
+        int temp = lower_bound(lis, lis + min_lines, v[i].second) - lis;
         lis[temp] = v[i].second;
-        pnt[v[i].second] = temp + 1;
+        lis_loca[v[i].second] = temp + 1;
         if (min_lines < temp + 1) min_lines = temp + 1;
     }
 
     cout << n - min_lines << "\n";
     int temp = min_lines;
     for (int i = max_num;i > 0;i--) {
-        while (pnt[i] != temp && i > 0) i--;
+        while (lis_loca[i] != temp && i > 0) i--;
         check[i] = 0;
         temp--;
         if (temp == 0) break;
