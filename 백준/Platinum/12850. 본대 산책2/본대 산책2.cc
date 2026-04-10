70745311b4d7a5701317069e7df12d069702f76d
limeunhyuk <68279740+limeunhyuk@users.noreply.github.com>
2026-04-10 11:47:50 +0900

    [Platinum V] Title: 본대 산책2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/12850. 본대 산책2/본대 산책2.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+const long long MAX = 1000000007;
+long long D;
+
+struct Matrix {
+    long long v[8][8];
+
+    Matrix() { memset(v, 0, sizeof(v)); }
+
+    Matrix operator*(const Matrix& B) const {
+        Matrix A;
+        for (int i = 0;i < 8;i++) {
+            for (int j = 0;j < 8;j++) {
+                for (int k = 0;k < 8;k++) {
+                    A.v[i][j] = (A.v[i][j] + v[i][k] * B.v[k][j]) % MAX;
+                }
+            }
+        }
+        return A;
+    }
+};
+
+Matrix matmul(Matrix a, int d) {
+    Matrix ans;
+    for (int i = 0;i < 8;i++) ans.v[i][i] = 1;
+
+    while (d) {
+        if (d % 2) ans = ans * a;
+        a = a * a;
+        d /= 2;
+    }
+
+    return ans;
+}
+
+long long board[8][8] = {
+    { 0,1,1,0,0,0,0,0 },
+    { 1,0,1,1,0,0,0,0 },
+    { 1,1,0,1,1,0,0,0 },
+    { 0,1,1,0,1,1,0,0 },
+    { 0,0,1,1,0,1,1,0 },
+    { 0,0,0,1,1,0,0,1 },
+    { 0,0,0,0,1,0,0,1 },
+    { 0,0,0,0,0,1,1,0 }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> D;
+
+    Matrix mat;
+    memcpy(mat.v, board, sizeof(board));
+
+    Matrix ans = matmul(mat, D);
+
+    cout << ans.v[0][0];
+    
+    return 0;
+}
